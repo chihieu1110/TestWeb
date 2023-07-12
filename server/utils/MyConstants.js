0d955061f52fc2a277eb30ea5e0ec33fb750b6d4
Hieu <chihieu20112001@gmail.com>
2023-07-12 09:08:36 +0700

    Add 3 project
    aaa

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.fh3krbh.mongodb.net',
+    DB_USER: 'hieu2',
+    DB_PASS: 'alo123',
+    DB_DATABASE: 'shoppingonline',
+    EMAIL_USER: 'chihieu20112001@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'gamjfftyogbwqpde',
+    JWT_SECRET: 'secret123',
+    JWT_EXPIRES: '31536000000', // in milliseconds
+  };
+  module.exports = MyConstants;
